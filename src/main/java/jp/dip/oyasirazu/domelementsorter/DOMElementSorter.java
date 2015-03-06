2725e1f9e2406f2ffcc0096f3b11942fe9aa223e
mikoto2000 <mikoto2000@gmail.com>
2015-03-06 03:43:07 +0000

    Added "set exclude element" mechanism.

--- src/main/java/jp/dip/oyasirazu/domelementsorter/DOMElementSorter.java
+++ src/main/java/jp/dip/oyasirazu/domelementsorter/DOMElementSorter.java
@@ -1,20 +1,33 @@
 package jp.dip.oyasirazu.domelementsorter;
 
+import java.io.IOException;
+import java.io.StringWriter;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * DOMElementSorter
  */
 public class DOMElementSorter {
 
-    public static final Comparator<Node> COMPARATOR_DEFAULT =  new Comparator<Node>() {
+    public static final NodeComparator NODE_COMPARATOR_DEFAULT =  new NodeComparator() {
         @Override
         public int compare(Node n1, Node n2) {
             // タグ名でソート
@@ -22,7 +35,15 @@ public class DOMElementSorter {
         }
     };
 
-    public static final SortCondition SORT_CONDITION_DEFAULT =  new SortCondition() {
+    public static final ExcludeTargetCondition EXCLUDE_TARGET_CONDITION_DEFAULT =  new ExcludeTargetCondition() {
+        @Override
+        public boolean isExcludeTarget(Node node) {
+            // 除外するノードなし
+            return false;
+        }
+    };
+    public static final SortTargetCondition SORT_CONDITION_DEFAULT =  new SortTargetCondition() {
+
         @Override
         public boolean isSortTarget(Node node) {
             return true;
@@ -30,14 +51,14 @@ public class DOMElementSorter {
     };
 
     public static void sort(Document document) {
-        sort(document, true, SORT_CONDITION_DEFAULT, COMPARATOR_DEFAULT);
+        sort(document, true, SORT_CONDITION_DEFAULT, NODE_COMPARATOR_DEFAULT, EXCLUDE_TARGET_CONDITION_DEFAULT);
     }
 
-    public static void sort(Element element, SortCondition sortCondition, Comparator<Node> comparator) {
-        sort(element, true, sortCondition, comparator);
+    public static void sort(Element element, SortTargetCondition SortTargetCondition, Comparator<Node> comparator, ExcludeTargetCondition excludeTargetCondition) {
+        sort(element, true, SortTargetCondition, comparator, excludeTargetCondition);
     }
 
-    private static void sort(Node node, boolean isRecursion, SortCondition sortCondition, Comparator<Node> comparator) {
+    private static void sort(Node node, boolean isRecursion, SortTargetCondition SortTargetCondition, Comparator<Node> comparator, ExcludeTargetCondition excludeTargetCondition) {
         // 子ノード情報取得
         NodeList nodes = node.getChildNodes();
         int size = nodes.getLength();
@@ -45,12 +66,12 @@ public class DOMElementSorter {
         // 再帰フラグが立って入れば、再帰する
         if (isRecursion) {
             for (int i = 0; i < size; i++) {
-                sort(nodes.item(i), isRecursion, sortCondition, comparator);
+                sort(nodes.item(i), isRecursion, SortTargetCondition, comparator, excludeTargetCondition);
             }
         }
 
         // ソートターゲットでなければ何もしない
-        if (!sortCondition.isSortTarget(node)) {
+        if (!SortTargetCondition.isSortTarget(node)) {
             return;
         }
 
@@ -68,7 +89,44 @@ public class DOMElementSorter {
         }
     }
 
-    public interface SortCondition {
+    /**
+     * ソート対象ノードを判定するためのインターフェース。
+     *
+     * isSortTarget(Node node) の戻り値が true であるノードの子要素をソートする。
+     */
+    public interface SortTargetCondition {
         boolean isSortTarget(Node node);
+    }
+
+    /**
+     * 出力対象外ノードを判定するためのインターフェース。
+     *
+     * isExcludeTarget(Node node) の戻り値が true であるノードは出力しない。
+     */
+    public interface ExcludeTargetCondition {
+        boolean isExcludeTarget(Node node);
+    }
+
+    /**
+     * ノードのソートに利用する Comparator。
+     */
+    public interface NodeComparator extends Comparator<Node> {}
+
+    public static class Util {
+        public static Document createDocument(String filePath) throws SAXException,
+                    IOException, ParserConfigurationException {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder.parse(filePath);
+        }
+
+        public static String documentToString(Document document) throws TransformerException {
+            StringWriter sw = new StringWriter();
+            TransformerFactory tfactory = TransformerFactory.newInstance();
+            Transformer transformer = tfactory.newTransformer();
+
+            transformer.transform(new DOMSource(document), new StreamResult(sw));
+            return sw.toString().replaceAll("\\>\\<", ">\n<").replaceAll("\\n\\s+", "\n");
+        }
     }
 }
--- src/main/java/jp/dip/oyasirazu/domelementsorter/Main.java
+++ src/main/java/jp/dip/oyasirazu/domelementsorter/Main.java
@@ -1,21 +1,11 @@
 package jp.dip.oyasirazu.domelementsorter;
 
 import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Comparator;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 public class Main {
@@ -23,27 +13,11 @@ public class Main {
     public static void main(String[] args) throws SAXException,
                 IOException, ParserConfigurationException, TransformerException {
 
-        Document document = createDocument(args[0]);
+        Document document = DOMElementSorter.Util.createDocument(args[0]);
         sortChildNode(document);
-        String documentString = documentToString(document);
+        String documentString = DOMElementSorter.Util.documentToString(document);
 
         System.out.println(documentString);
-    }
-
-    private static Document createDocument(String filePath) throws SAXException,
-                IOException, ParserConfigurationException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        return builder.parse(filePath);
-    }
-
-    private static String documentToString(Document document) throws TransformerException {
-        StringWriter sw = new StringWriter();
-        TransformerFactory tfactory = TransformerFactory.newInstance();
-        Transformer transformer = tfactory.newTransformer();
-
-        transformer.transform(new DOMSource(document), new StreamResult(sw));
-        return sw.toString().replaceAll("\\>\\<", ">\n<").replaceAll("\\n\\s+", "\n");
     }
 
     private static void sortChildNode(Document document) {
