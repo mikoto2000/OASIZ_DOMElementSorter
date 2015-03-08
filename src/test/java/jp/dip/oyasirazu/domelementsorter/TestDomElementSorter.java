ca154dbb70c91aba3f9040ce95aecf5409ee8d26
mikoto2000 <mikoto2000@gmail.com>
2015-03-08 09:58:11 +0000

    Added first test case.

--- src/test/java/jp/dip/oyasirazu/domelementsorter/TestDomElementSorter.java
+++ src/test/java/jp/dip/oyasirazu/domelementsorter/TestDomElementSorter.java
@@ -1,13 +1,32 @@
-import static org.junit.Assert.*;
+package jp.dip.oyasirazu.domelementsorter;
+
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 /**
  * TestDomElementSorter
  */
 public class TestDomElementSorter {
+    private static final String EMPTY_XML_PATH =
+        "src/test/resource/EmptyXml.xml";
+    private static final String EMPTY_XML_OUTPUT =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<xml/>";
+
     @Test
-    public void testSort() {
-        
+    public void testSortEmptyXml() throws SAXException, ParserConfigurationException, TransformerException, IOException {
+        Document document = DOMElementSorter.Util.createDocument(EMPTY_XML_PATH);
+        DOMElementSorter.sort(document);
+        String result = DOMElementSorter.Util.documentToString(document);
+
+        assertThat(result, is(EMPTY_XML_OUTPUT));
     }
 }
