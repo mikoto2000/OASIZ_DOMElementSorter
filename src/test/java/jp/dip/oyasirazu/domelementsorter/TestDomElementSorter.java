756c959e9ce22c6e245e52a86379e900eed42e1d
mikoto2000 <mikoto2000@gmail.com>
2015-03-09 00:08:23 +0900

    Added test.
    下記機能のテストを追加。
    
    - ソート順の指定
    - ソート対象ノードの指定
    - 出力対象外ノードの指定

--- src/test/java/jp/dip/oyasirazu/domelementsorter/TestDomElementSorter.java
+++ src/test/java/jp/dip/oyasirazu/domelementsorter/TestDomElementSorter.java
@@ -24,6 +24,18 @@ public class TestDomElementSorter {
         "src/test/resource/TagName.xml";
     private static final String TAG_NAME_OUTPUT =
             "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<xml>\n<a>gast.</a>\n<b>test.</b>\n<c>masg.</c>\n</xml>";
+    private static final String SORT_CONDITION_PATH =
+        "src/test/resource/SortCondition.xml";
+    private static final String SORT_CONDITION_OUTPUT =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<xml>\n<notarget>\n<b>test.</b>\n<c>masg.</c>\n<a>gast.</a>\n</notarget>\n<target>\n<a>gast.</a>\n<b>test.</b>\n<c>masg.</c>\n</target>\n</xml>";
+    private static final String NODE_COMPARATOR_PATH =
+        "src/test/resource/TagName.xml";
+    private static final String NODE_COMPARATOR_OUTPUT =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<xml>\n<c>masg.</c>\n<b>test.</b>\n<a>gast.</a>\n</xml>";
+    private static final String EXCLUDE_CONDITION_PATH =
+        "src/test/resource/ExcludeCondition.xml";
+    private static final String EXCLUDE_CONDITION_OUTPUT =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<xml>\n<target>\n<a>gast.</a>\n<b>test.</b>\n<c>masg.</c>\n</target>\n</xml>";
 
     @Test
     public void testSortEmptyXml() throws SAXException, ParserConfigurationException, TransformerException, IOException {
@@ -41,5 +53,61 @@ public class TestDomElementSorter {
         String result = DOMElementSorter.Util.documentToString(document);
 
         assertThat(result, is(TAG_NAME_OUTPUT));
+    }
+
+    @Test
+    public void testSortWithDetailConfigSortCondition() throws SAXException, ParserConfigurationException, TransformerException, IOException {
+        Document document = DOMElementSorter.Util.createDocument(SORT_CONDITION_PATH);
+        DOMElementSorter.sort(
+                document,
+                (node) -> {
+                    // タグ名「notarget」下はソートしない
+                    if (node.getNodeName().equals("notarget")) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                },
+                DOMElementSorter.NODE_COMPARATOR_DEFAULT,
+                DOMElementSorter.EXCLUDE_TARGET_CONDITION_DEFAULT);
+        String result = DOMElementSorter.Util.documentToString(document);
+
+        assertThat(result, is(SORT_CONDITION_OUTPUT));
+    }
+
+    @Test
+    public void testSortWithDetailConfigNodeComparator() throws SAXException, ParserConfigurationException, TransformerException, IOException {
+        Document document = DOMElementSorter.Util.createDocument(NODE_COMPARATOR_PATH);
+        DOMElementSorter.sort(
+                document,
+                DOMElementSorter.SORT_CONDITION_DEFAULT,
+                (n1, n2) -> {
+                    // タグ名の降順でソート
+                    return n2.getNodeName().compareTo(n1.getNodeName());
+                },
+                DOMElementSorter.EXCLUDE_TARGET_CONDITION_DEFAULT);
+        String result = DOMElementSorter.Util.documentToString(document);
+
+        assertThat(result, is(NODE_COMPARATOR_OUTPUT));
+    }
+
+    @Test
+    public void testSortWithDetailConfigExcludeCondition() throws SAXException, ParserConfigurationException, TransformerException, IOException {
+        Document document = DOMElementSorter.Util.createDocument(EXCLUDE_CONDITION_PATH);
+        DOMElementSorter.sort(
+                document,
+                DOMElementSorter.SORT_CONDITION_DEFAULT,
+                DOMElementSorter.NODE_COMPARATOR_DEFAULT,
+                (node) -> {
+                    // タグ名「notarget」下は出力しない
+                    if (node.getNodeName().equals("notarget")) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+        String result = DOMElementSorter.Util.documentToString(document);
+
+        assertThat(result, is(EXCLUDE_CONDITION_OUTPUT));
     }
 }
